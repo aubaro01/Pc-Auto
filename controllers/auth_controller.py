88e7d9c45e1626d73b847d2899a1dedad3b47974
Cláudio Barroso <claudiobarroso106@gmail.com>
2025-02-18 18:57:19 +0000

    fix

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,17 +1,10 @@
 from models.admin import get_user_by_username, verify_user_password
-import logging
 
 class AuthController:
     def __init__(self):
-        # Configuração do logger
-        self.logger = logging.getLogger(__name__)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
-        self.logger.setLevel(logging.INFO)
+        pass
 
-    def login(self, username, password, show_success_message=False):
+    def login(self, username, password):
         if not username or not password:
             return False, "Os campos de username e senha devem ser preenchidos."
 
@@ -19,15 +12,11 @@ class AuthController:
             user = get_user_by_username(username)
             if user:
                 if verify_user_password(user, password):
-                    if show_success_message:
-                        return True, "Login realizado com sucesso!"
-                    else:
-                        return True, None
+                    return True, "Login realizado com sucesso!"
                 else:
                     return False, "Senha incorreta. Tente novamente."
             else:
                 return False, "Usuário não encontrado. Tente novamente."
         except Exception as e:
-            self.logger.error(f"Erro no processo de login: {e}")
+            print(f"Erro no processo de login: {e}")
             return False, "Erro no processo de login. Tente novamente."
-

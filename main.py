9e3e39f43be71c983188f83d6c4ed0fec3a5c230
Cláudio Barroso <claudiobarroso106@gmail.com>
2025-04-11 23:10:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ from views.auth_view import AuthView
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("blue")
 
+
+
+
+
+def mostrar_informacoes(veiculo):
+    print(veiculo.exibir_info())
+
+carro = Carro("Toyota", "Corolla", 2020, 4)
+moto = Moto("Honda", "ddd", 2019, 500)
+
+mostrar_informacoes(carro)  
+mostrar_informacoes(moto)   
+
+
 if __name__ == "__main__":
     app = AuthView()
     app.mainloop()
+
+    
